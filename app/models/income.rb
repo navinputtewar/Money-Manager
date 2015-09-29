a3679c8af81f7bbdf8b09dd5f5e6701e735d75f5
Navin <newd31@gmail.com>
2015-09-29 17:26:33 +0530

    callback used for transaction creation

--- app/models/income.rb
+++ app/models/income.rb
@@ -3,8 +3,15 @@ class Income < ActiveRecord::Base
   has_many :transactions, as: :transable, dependent: :destroy
   validates :category, :format => { :with => /\A[A-Za-z0-9' ][A-Za-z0-9' ]*\z/, message: "Only allowed letters, numbers and '" }, presence: true
   validates :amount, :format => { :with => /\A[0-9.0-9 ][0-9.0-9' ]*\z/, message: "Only allowed numbers and decimal" }, presence: true
-
+  after_create :create_transaction
+  
   def self.like(filter)
   	Income.where("category like ?", "%#{filter}%")
   end
+  
+  def create_transaction
+    Transaction.create(date: params[:income][:date])
+  end
+  
 end
+
